3dc7eb5274fe253ecf7d7defd2075497ed86df58
Jeremy Wood <farachan@gmail.com>
2017-08-24 21:22:03 -0400

    Added config lib.

--- bot/src/main/kotlin/kaddy/MappedCollection.kt
+++ bot/src/main/kotlin/kaddy/MappedCollection.kt
@@ -1,9 +1,12 @@
 package kaddy
 
+/**
+ * A special collection that maps all of its elements to a key of some type.
+ */
 interface MappedCollection<in KeyType, out ElementType> : Collection<ElementType> {
 
     /**
-     * Returns a member of this collection that is mapped to the given key.
+     * Returns a member of this collection that is mapped to the given key (or id).
      *
      * @param key The key that represents a possible member of this collection.
      * @return The element mapped to the given key if the key exists, otherwise null.
