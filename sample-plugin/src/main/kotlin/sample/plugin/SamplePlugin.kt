e596e4e8ff792edc268ca46941cb1ad7a07ccd91
Jeremy Wood <farachan@gmail.com>
2017-08-26 20:34:35 -0400

    Added an event handler to sample plugin.

--- sample-plugin/src/main/kotlin/sample/plugin/SamplePlugin.kt
+++ sample-plugin/src/main/kotlin/sample/plugin/SamplePlugin.kt
@@ -1,14 +1,23 @@
 package sample.plugin
 
+import kaddy.event.EventHandler
+import kaddy.event.Listener
+import kaddy.event.channel.ChannelChangeNameEvent
 import kaddy.plugin.java.JavaPlugin
 
-class SamplePlugin : JavaPlugin() {
+class SamplePlugin : JavaPlugin(), Listener {
 
     override fun onEnable() {
+        bot.pluginManager.registerEvents(this, this)
         logger.info { "Hi!" }
     }
 
     override fun onDisable() {
         logger.info { "Bye!" }
+    }
+
+    @EventHandler
+    fun channelChangeName(event: ChannelChangeNameEvent) {
+        event.channel.sendMessage("WOAH BUDDY")
     }
 }
